737384d23c73a542dfcb57b873abc42109b342d5
shanzi <yun.er.run@gmail.com>
2011-09-25 22:25:15 +0800

    commit settings

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
