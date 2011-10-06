97146322886b827f7f3999fda9e4db802b7cd871
shanzi <yun.er.run@gmail.com>
2011-10-06 20:42:28 +0800

    放弃clog计划

--- clog/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from words.clog.models import *
-admin.site.register(Log)
-admin.site.register(Comment)
-
--- clog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-from words.clog.views import *
-
-
-
-urlpatterns = patterns('',
-        url(r'^$',index),
-        url(r'^search/',search),
-        )
